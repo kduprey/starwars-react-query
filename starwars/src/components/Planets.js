31ec13194bd08a4c2bb87c8ab2fb99d4ef35cc70
Kenton Duprey <kentonduprey@icloud.com>
2021-12-17 12:34:11 -0700

    adding query logic

--- starwars/src/components/Planets.js
+++ starwars/src/components/Planets.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "react-query";
+import Planet from "./Planet";
+
+const fetchPlanets = async () => {
+	const res = await fetch("https://www.swapi.tech/api/planets/");
+	return res.json();
+};
 
 const Planets = () => {
-  return (
-    <div>
-      <h2>Planets</h2>
-    </div>
-  );
-}
- 
+	const { data, status } = useQuery("planets", fetchPlanets);
+
+	console.log(data);
+
+	return (
+		<div>
+			<h2>Planets</h2>
+			{/* <p>{status}</p> */}
+
+			{status === "success" && (
+				<div>
+					{data.results.map((planet) => (
+						<Planet planet={planet} key={planet.name} />
+					))}
+				</div>
+			)}
+
+			{status === "loading" && <div>Loading data...</div>}
+
+			{status === "error" && <div>Error fetching data</div>}
+		</div>
+	);
+};
+
 export default Planets;
